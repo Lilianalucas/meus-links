ef34750824987c5e3e90acb9733658cc335a2fa8
Liliana <140682991+Lilianalucas@users.noreply.github.com>
2026-04-18 09:44:45 -0300

    site links

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  //pegar a teg imagem
+  const img = document.querySelector("#profile img");
+  //se tiver light mode, adicione a imagem light
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/foto.png");
+  } else {
+    // se tiver sem light mode, adicione a imagem normal
+    img.setAttribute("src", "./assets/link-foto.png");
+  }
+}
