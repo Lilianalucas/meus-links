2450bd56d9e323031616df890949a2a18e8c931e
liliana lucas <lilianalucas48@gmail.com>
2026-04-21 15:00:42 -0300

    Correção: remover código duplicado no script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function toggleMode() {
   const html = document.documentElement;
   html.classList.toggle("light");
 
-  //pegar a teg imagem
+  //pegar a tag imagem
   const img = document.querySelector("#profile img");
   //se tiver light mode, adicione a imagem light
   if (html.classList.contains("light")) {
@@ -12,15 +12,3 @@ function toggleMode() {
     img.setAttribute("src", "./assets/link-foto.png");
   }
 }
-
-  //pegar a teg imagem
-  const img = document.querySelector("#profile img");
-  //se tiver light mode, adicione a imagem light
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/foto.png");
-  } else {
-    // se tiver sem light mode, adicione a imagem normal
-    img.setAttribute("src", "./assets/link-foto.png");
-  }
-}
->>>>>>> 5e0d6d5d8cb3315ab83f0b67d00b09ddb61dadad
